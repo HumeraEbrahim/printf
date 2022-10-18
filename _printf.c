bee631e24106212d10b57fa8c4deb3c350658bd3
Ntombikayise <gimmymtshali@gmail.com>
2022-10-18 10:13:15 -0700

    0. I'm not going anywhere. You can print that wherever you want to. I'm here and I'm a Spur for life

--- _printf.c
+++ _printf.c
@@ -1,56 +1,35 @@
-#include <stdarg.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
-#include <stddef.h>
 
 /**
- * _printf - recreates the printf function
+ * _printf - prints according to a given format. printf Implementation.
  *
- * @format: string with format specifier
+ * @format: string holding characters and format of arguments to be printed.
  *
- * Return: number of characters printed
+ * Return: number of charaters printed.
  */
 
 int _printf(const char *format, ...)
 {
-	if (format != NULL)
-	{
-		int count = 0, i;
-		int (*m)(va_list);
-		va_list args;
+	va_list original, copy;
+	int n_printed = 0, error = 0;
 
-		va_start(args, format);
-		i = 0;
-		if (format[0] == '%' && format[1] == '\0')
-			return (-1);
-		while (format != NULL && format[i] != '\0')
-		{
-			if (format[i] == '%')
-			{
-				if (format[i + 1] == '%')
-				{
-					count += _putchar(format[i]);
-					i += 2;
-				}
-				else
-				{
-					m = get_func(format[i + 1]);
-					if (m)
-						count += m(args);
-					else
-						count = _putchar(format[i]) + _putchar(format[i + 1]);
-					i += 2;
-				}
-			}
-			else
-			{
-				count += _putchar(format[i]);
-				i++;
-			}
-		}
-		va_end(args);
-		return (count);
+	if (format == NULL || *format == '\0')
+		return (-1);
+
+	va_start(original, format);
+	va_copy(copy, original);
+
+	error = check_input(format, &original);
+
+	if (error != 0)
+	{
+		return (error * -1);
 	}
-	return (-1);
+
+	n_printed = print_output(format, &copy);
+
+	va_end(original);
+	va_end(copy);
+
+	return (n_printed);
 }
